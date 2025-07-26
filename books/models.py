0da9280f852f5922c72b52bef234c734ce77adcf
NarendarTrainer <narendartrainer@gmail.com>
2025-07-26 15:31:33 +0530

    model is added

--- books/models.py
+++ books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title  = models.CharField(max_length = 200)
+    author = models.CharField(max_length = 200)
+    description = models.CharField(max_length = 500, default=None)
+    price = models.FloatField(null=True, blank=True)
+    image_url = models.CharField(max_length = 2083, default=False)
+    follow_author = models.CharField(max_length=2083, blank=True)  
+    book_available = models.BooleanField(default=False)
+
